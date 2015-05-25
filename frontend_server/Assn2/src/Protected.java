051305d63259c39e6929a18bf2c9fef8f95bbe07
Adam Chalmers <adam.s.chalmers@gmail.com>
2015-05-25 15:51:24 +1000

    Setting up form auth.

--- frontend_server/Assn2/src/Protected.java
+++ frontend_server/Assn2/src/Protected.java
@@ -2,29 +2,23 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Survey
- * This class handles survey submitted by the user and displays results in another JSP
- */
 
-@WebServlet("/index.html")
-public class Index extends HttpServlet {
+public class Protected extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	RequestDispatcher view = request.getRequestDispatcher("/index.jsp");
-    	/*if (request.isUserInRole("admin")) {
+    	RequestDispatcher view = request.getRequestDispatcher("/protected.jsp");
+    	if (request.isUserInRole("admin")) {
         	request.setAttribute("message", "Welcome, administrator.");
     	} else {
-    		request.setAttribute("message", "Server message working!");
+    		request.setAttribute("message", "Welcome, user.");
     	}
-    	request.setAttribute("username", request.getUserPrincipal().getName());*/
+    	request.setAttribute("username", request.getUserPrincipal().getName());
 		view.forward(request,response);
 	}
 
